7169236b96ed526099033aa77f33c804a8ac8267
EC2 Default User <ec2-user@ip-172-31-84-35.ec2.internal>
2020-05-04 11:43:01 +0000

    initalize project with first recipe component

---
+++ src/RecipeApp.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './RecipeApp.css';
+import Recipe from './Recipe';
+
+
+class RecipeApp extends Component {
+  
+  render() {
+    return (
+    <div className="App">
+      <Recipe 
+        title="pasta" 
+        ingredients={["flour", "water"]}/>
+    </div>
+  );  
+}
+  
+
+}
+
+export default RecipeApp;
