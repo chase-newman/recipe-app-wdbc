c0c02ca709c8ce22523ed5395261615244a8155a
EC2 Default User <ec2-user@ip-172-31-84-35.ec2.internal>
2020-05-04 12:03:44 +0000

    create first recipe card

--- src/RecipeApp.js
+++ src/RecipeApp.js
@@ -10,7 +10,9 @@ class RecipeApp extends Component {
     <div className="App">
       <Recipe 
         title="pasta" 
-        ingredients={["flour", "water"]}/>
+        ingredients={["flour", "water"]}
+        instructions="Mix ingredients"
+        img="https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg" />
     </div>
   );  
 }
